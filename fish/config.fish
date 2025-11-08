876507cb7be9632ea1dff1055e61f21ea5432739
Konstantin Kharchilava <kote@Konstantins-MacBook-Air.local>
2025-11-09 01:03:20 +0400

    removed bs

--- fish/config.fish
+++ fish/config.fish
@@ -15,35 +15,6 @@ if status is-interactive
         echo -n "> "
     end
 
-    # Helix automatic theme sync script
-
-    function sync_helix_theme
-        set current_theme (defaults read -g AppleInterfaceStyle 2>/dev/null)
-        set config_path "$HOME/.config/helix/config.toml"
-
-        if test -f $config_path
-            if test "$current_theme" = Dark
-                set new_theme flexoki_dark
-            else
-                set new_theme flexoki_light
-            end
-
-            set current_helix_theme (grep "^theme = " $config_path | sed 's/theme = "\(.*\)"/\1/')
-
-            if test "$current_helix_theme" != "$new_theme"
-                sed -i '' "s/^theme = .*/theme = \"$new_theme\"/" $config_path
-            end
-        end
-    end
-
-    # Run on shell startup
-    sync_helix_theme
-
-    # Auto-check on command execution
-    function __helix_theme_check --on-event fish_preexec
-        sync_helix_theme
-    end
-
     set -gx PATH $PATH /Users/kote/Library/pnpm
     set -gx PATH $PATH ~/.local/share/bob/nvim-bin
 
